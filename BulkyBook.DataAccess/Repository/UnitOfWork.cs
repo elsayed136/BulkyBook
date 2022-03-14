125fabbf9e681aae46cee5f93f16580fbc04292b
elsayed136 <Elsayed_Ahmed_Eltaweel@outlook.com>
2022-03-14 02:23:45 +0200

    Add: Implementing Unit of Work

---
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+        }
+        public ICategoryRepository Category { get; }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
