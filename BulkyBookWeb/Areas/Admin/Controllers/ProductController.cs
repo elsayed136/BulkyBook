743c2b22878211b2e1b98e5842f65ef89127b2f5
elsayed136 <Elsayed_Ahmed_Eltaweel@outlook.com>
2022-03-15 19:02:40 +0200

    Add: Upsert GET functionality to Add product

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BulkyBookWeb.Areas.Admin.Controllers
 {
@@ -16,6 +18,46 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
         {
             IEnumerable<Product> productList = _unitOfWork.Product.GetAll();
             return View(productList);
+        }
+        // Upsert(Update/Create)
+        // GET Product/Upsert
+        public IActionResult Upsert(int? id)
+        {
+            ProductVM productVM = new()
+            {
+                Product = new(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+                CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+            };
+            if (id == null || id == 0)
+            {
+                // create product
+                return View(productVM);
+            }
+            else
+            {
+                // update product
+            }
+
+            return View();
+        }
+        // POST Product/Upsert
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(ProductVM obj,IFormFile file)
+        {
+            if (!ModelState.IsValid) return View();
+
+            TempData["success"] = "Product Created successfully";
+            return View();
         }
 
     }
