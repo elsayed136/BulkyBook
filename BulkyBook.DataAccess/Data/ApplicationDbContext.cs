7cad5591441c6ae4f98f99403e803335837dce7a
elsayed136 <Elsayed_Ahmed_Eltaweel@outlook.com>
2022-03-15 15:52:59 +0200

    Add: Product Table to Db

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -11,6 +11,7 @@ namespace BulkyBook.DataAccess
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<CoverType> CoverTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
