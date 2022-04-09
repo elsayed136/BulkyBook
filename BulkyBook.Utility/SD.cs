71dde77f2f86a5b6a41cddc50ebe05cbd74d72c5
elsayed136 <Elsayed_Ahmed_Eltaweel@outlook.com>
2022-04-09 23:30:16 +0200

    Add: Roles for website.

--- BulkyBook.Utility/SD.cs
+++ BulkyBook.Utility/SD.cs
@@ -4,5 +4,9 @@ namespace BulkyBook.Utility
     // for the static details
     public static class SD
     {
+        public const string RoleUserIndividual = "Individual";
+        public const string RoleUserCompany = "Company";
+        public const string RoleAdmin = "Admin";
+        public const string RoleEmployee = "Employee";
     }
 }
